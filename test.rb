2c9e4be3ff5ef7a586da352dd4458273573f64ef
Aanand Prasad <aanand.prasad@gmail.com>
2011-01-04 13:48:49 +0000

    Modify test.rb to take filenames on the command line

--- test.rb
+++ test.rb
@@ -3,20 +3,37 @@
 require 'yaml'
 require 'Stata'
 
-[
-  'test_file_1',
-  'test_file_2',
-  'test_file_3',
-].each do |file|
+files = if ARGV.empty?
+          Dir.glob("test_file_*.dta")
+        else
+          ARGV
+        end
+
+if files.empty?
+  puts "usage: #{$0} file1.dta [file2.dta ...]"
+  exit(-1)
+end
+
+files.each do |original|
+  if original =~ /_resave/
+    puts "skipping #{original.inspect}\n\n"
+    next
+  end
+
+  ext    = File.extname(original)
+  base   = original[0..-(ext.length+1)]
+  resave = "#{base}_resave#{ext}"
   
-  s1 = Stata.read "#{file}.dta"
-  Stata.write "#{file}_resave.dta", s1
+  s1 = Stata.read(original)
+  Stata.write(resave, s1)
   
-  s2 = Stata.read "#{file}_resave.dta"
+  s2 = Stata.read(resave)
+
   ['data', 'fields', 'value_labels'].each do |f|
     if (s1[f] != s2[f])
       puts "ERROR '#{file}' #{f} did not read/write the same!"
     end
   end
+
   puts "\n"
 end
